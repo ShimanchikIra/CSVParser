b6dc4378788516df7010ac29188c0e185a74726d
ShimanchikIra <shimanchikirina@gmail.com>
2019-07-07 14:24:11 +0300

    add parse csv-file

--- app.ts
+++ app.ts
@@ -1,2 +1,11 @@
+import fs = require("fs");
+import  csvParser = require('csv-parser');
+// import config = require('./config');
+
+fs.createReadStream('Users.csv')
+    .pipe(csvParser({ separator: ';' }))
+    .on('data', (data) => { console.log(data)})//{validate(data, config.csv);outputResult(validate, data, config.csv)})
+    .on('end', () => {console.log('end');
+    });
 
 
