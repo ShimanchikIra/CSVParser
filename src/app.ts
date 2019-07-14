05153c0c029603bc14fb556be189b6ae5044fa6c
ShimanchikIra <shimanchikirina@gmail.com>
2019-07-15 02:12:56 +0300

    fix validate
    *before checked each received user
    *now filter the received array of users

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,58 @@
 import csvParser = require('csv-parser');
 import fs = require("fs");
-import {csv, ColumnDescriptor} from "./config";
-
+// import {csv} from "./config";
+import config = require('./config');
+const results = [];
 
 fs.createReadStream('src/Users.csv')
     .pipe(csvParser({ separator: ';' }))
-    .on('data', (data) => {validateObj(data, csv).then(errors=>{
-        if (errors.length>0){
-            console.log(`\nInvalid data: \n${JSON.stringify(data, null, 1)}`, ...errors);
-            fs.appendFileSync("src/InvalidData", `\n${JSON.stringify(data, null, 0)} ${errors}\n`);
-        } else {
-            fs.appendFileSync("src/ValidData", `${JSON.stringify(data, null, 0)}\n`);
-        }
-    }).catch(function (error) {
-        console.log(error.message);
-    })
+    .on('data',(data)=>{results.push(data)})
+    .on('end',()=>{fs.appendFileSync("src/ValidData", writeToValidFile(results.filter(value => isValidObj(value))));
     });
 
-
+function isValidObj(value:User): boolean{
+    let isValid:boolean=true;
+    let errorsObj: string[] = [];
+    for (let key in value) {
+        let errorsKey:string[]=validation(key, value[key]);
+        if (errorsKey.length)
+            errorsObj.push(`\n${key}: `, ...errorsKey);
+    }
+    if (errorsObj.length){
+        isValid=false;
+        console.log(`\nInvalid data: \n${JSON.stringify(value, null, 1)}`, ...errorsObj);
+        fs.appendFileSync("src/InvalidData", `\n${JSON.stringify(value, null, 1)} ${errorsObj}\n`);
+    }
+    return isValid;
+}
+function validation(key: string, value: string): string[]{
+    let errorsKey: string[] = [];
+    config.csv.forEach((columnDescriptor) => {
+        if (key === columnDescriptor.name) {
+            columnDescriptor.validators.forEach((validator) =>
+             {
+                if (validator.validate(value).length)
+                    errorsKey.push(...validator.validate(value));
+            })
+        }
+    });
+    return errorsKey;
+}
 interface User {
-    ID:string,
-    Name:string,
-    Surname:string,
-    Mail:string,
-    'Date of registration':string,
-    Phone:string;
+    ID: string,
+    Name: string,
+    Surname: string,
+    Mail: string,
+    'Date of registration': string,
+    Phone: string;
 }
-function validateObj(value: User, csv: ColumnDescriptor[]):Promise<string[]> {
-    return new Promise(function (resolve, reject) {
-        let errorsArr: string[] = [];
-        for (let key in value) {
-            for (let i = 0; i < csv.length; i++) {
-                if (value.hasOwnProperty(csv[i].name) && key === csv[i].name) {
-                    for (let j = 0; j < csv[i].validators.length; j++) {
-                        let errorsField: string[] = csv[i].validators[j].validate(value[key]);
-                        if (errorsField.length > 0)
-                            errorsArr.push(`\n${key}: `, ...errorsField);
-                    }
-                }
-            }
+function writeToValidFile(users: User[]): string {
+    let out: string=null;
+    users.forEach((user)=> {
+        for (let key in user) {
+            out +=`${user[key]},`;
         }
-        if (errorsArr)
-            resolve(errorsArr);
-        else {
-            let reason = new Error('validation not implemented');
-            reject(reason);
-        }
-    })
+        out +=`\n`;
+    });
+   return out;
 }
-
